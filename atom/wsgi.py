2084147145c46ba46d5a75a791df6599058766dc
shin-carpediem <buru.aoshin@gmail.com>
2021-03-08 09:35:29 +0900

    set postgre

---
+++ atom/wsgi.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for atom project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+from whitenoise.django import DjangoWhiteNoise
+from .settings import DEBUG
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atom.settings')
+
+application = get_wsgi_application()
+
+# Heroku
+if DEBUG == False:
+    application = DjangoWhiteNoise(application)
