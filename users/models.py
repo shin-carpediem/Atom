4fc2deaebbde447ad4666bc5da1dddaefb8eb0a7
shin-carpediem <buru.aoshin@gmail.com>
2021-03-09 08:34:42 +0900

    cp previous atom

--- users/models.py
+++ users/models.py
@@ -1,3 +1,62 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.core.mail import send_mail
+from django.utils import timezone
+
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    # カスタムユーザーマネージャー
+    use_in_migrations = True  # このクラスもマイグレーションで管理できるようになる
+
+    def _create_user(self, email, password, **extra_fields):
+        # emailを必須にする
+        if not email:
+            raise ValueError('The given email must be set.')
+        # emailでUserモデルを作成
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # カスタムユーザーモデル
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField("email", unique=True)
+    name = models.CharField(max_length=20, default="User")
+    # house = [
+    #     ("eifukutyo", "Eifukutyo"),
+    #     ("akasaka", "Akasaka"),
+    #     ("oyama-ikebukuro", "Oyama-Ikebukuro"),
+    # ]
+    is_active = models.BooleanField("is_active", default=False) # 仮登録状態→本登録でTrueにする
+    is_staff = models.BooleanField("is_staff", default=False)
+    date_joined = models.DateTimeField("date_joined", default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from re import template
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+app_name = 'users'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LoginView.as_view(), name=('logout')),
+]
