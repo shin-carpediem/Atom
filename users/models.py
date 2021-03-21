d417175ee5adb091994cb775a3e9585d7c3c362d
shin-carpediem <buru.aoshin@gmail.com>
2021-03-21 21:15:39 +0900

    fix model

--- users/models.py
+++ users/models.py
@@ -49,9 +49,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     # カスタムユーザーモデル
     email = models.EmailField("email", unique=True)
     name = models.CharField(max_length=20, default="ユーザー")
-    house = models.ForeignKey(House, on_delete=PROTECT, default=1)
+    house = models.ForeignKey(House, on_delete=PROTECT, null=True)
     housechore_title = models.CharField(max_length=100, default="not assigned")
-    housechore_desc = models.CharField(max_length=100, blank=True)
+    housechore_desc = models.CharField(max_length=100, blank=True, null=True)
     is_staff = models.BooleanField("is_staff", default=False)
     # 仮登録状態→本登録でTrueにする。
     is_active = models.BooleanField("is_active", default=True)
