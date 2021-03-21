b7451a252bf88888988624e778de576b10cf89c0
shin-carpediem <buru.aoshin@gmail.com>
2021-03-21 19:37:57 +0900

    make withdraw confirm

--- static/js/users/base.js
+++ static/js/users/base.js
@@ -12,3 +12,16 @@ const logOutConfirm = () => {
   });
 };
 logOutConfirm();
+
+const withdrawConfirm = () => {
+  const logout = document.getElementById("withdraw");
+
+  logout.addEventListener("click", () => {
+    const answer = confirm("アカウントが削除されます。本当に退会しますか?");
+    console.log(answer);
+    if (answer) {
+      window.location.href = "/withdraw/";
+    }
+  });
+};
+withdrawConfirm();
