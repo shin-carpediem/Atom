4c5886783ed8de656a24cf33e746d559e788f867
shin-carpediem <buru.aoshin@gmail.com>
2021-03-27 17:01:28 +0900

    fix model

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from django.db.models.deletion import CASCADE
 class HouseChore(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    house = models.ForeignKey(House, on_delete=CASCADE, null=True)
+    house = models.ForeignKey(House, on_delete=CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.title
