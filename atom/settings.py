ad50e33ae3d3ae0961a2cce67e5fac2756e8203a
shin-carpediem <buru.aoshin@gmail.com>
2021-04-17 09:33:27 +0900

    fix db

--- atom/settings.py
+++ atom/settings.py
@@ -133,7 +133,7 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': env("DB_NAME"),
             'USER': env("DB_USER"),
             'PASSWORD': env("DB_PASSWORD"),
